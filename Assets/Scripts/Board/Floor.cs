c440f16251246fcc32682279cc5ad0d92f70f6e4
Reichardt-G <guilhermeneves.reichardt@gmail.com>
2024-10-19 14:51:12 -0300

    First commit

---
+++ Assets/Scripts/Board/Floor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Floor : MonoBehaviour
+{
+    [HideInInspector]
+    public TilemapRenderer tilemapRenderer;        
+    public int order{get{
+        return tilemapRenderer.sortingOrder;
+    }}
+    public int contentOrder;
+
+    void Awake(){
+        tilemapRenderer = this.transform.GetComponent<TilemapRenderer>();
+    }
+}
